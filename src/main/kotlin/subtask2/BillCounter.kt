f2b17b13eff92a8dbab0d27127801f340dc0e034
fusion-steel <pavelgently@gmail.com>
2021-05-22 22:07:46 +0300

    Add Task1BillCounter solution.

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -1,10 +1,19 @@
 package subtask2
 
 class BillCounter {
-
-    // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
+    /**
+     * This function calculates correctness of given bill, which is IntArray,
+     * split between two persons, one person doesn't pay for item with index 'k'.
+     * If calculation is correct, returns String "Bon Appetit",
+     * otherwise - change that the first person should return to the second.
+     */
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        bill[k] = 0
+        val charged = bill.sum() / 2
+        return if (charged == b) {
+            "Bon Appetit"
+        } else {
+            (b - charged).toString()
+        }
     }
 }
