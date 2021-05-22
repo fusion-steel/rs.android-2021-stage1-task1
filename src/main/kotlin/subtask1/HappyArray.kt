a565b45925b705b067b281e3f1ad6dfd6a384664
fusion-steel <pavelgently@gmail.com>
2021-05-22 21:33:25 +0300

    Add Task1HappyArray solution.

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,16 @@
 package subtask1
 
 class HappyArray {
-
-    // TODO: Complete the following function
+    /**
+    This function converts sadArray to happyArray.
+    Elements considered 'bad' if the sum of nearest elements is less then its value.
+     Also the first and the last elements of the given array always considered 'happy'.
+     */
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val tempArray = sadArray.filterIndexed { index, item ->
+            index == 0 || index == sadArray.lastIndex || (sadArray[index - 1] + sadArray[index + 1] >= item)
+        }
+        return if (tempArray.size == sadArray.size) tempArray.toIntArray()
+        else convertToHappy(tempArray.toIntArray())
     }
 }
