6a93c3d9458b12b1d9051e4dcb2b4b9ad4b2cc7a
fusion-steel <pavelgently@gmail.com>
2021-05-22 22:08:55 +0300

    Docs: add stars in documentation strings.

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,15 +2,16 @@ package subtask1
 
 class HappyArray {
     /**
-    This function converts sadArray to happyArray.
-    Elements considered 'bad' if the sum of nearest elements is less then its value.
-     Also the first and the last elements of the given array always considered 'happy'.
+     * This function converts sadArray to happyArray.
+     * Elements considered 'bad' if the sum of nearest elements is less then its value.
+     * Also the first and the last elements of the given array always considered 'happy'
      */
     fun convertToHappy(sadArray: IntArray): IntArray {
         val tempArray = sadArray.filterIndexed { index, item ->
             index == 0 || index == sadArray.lastIndex || (sadArray[index - 1] + sadArray[index + 1] >= item)
         }
-        return if (tempArray.size == sadArray.size) tempArray.toIntArray()
+        return if (tempArray.size == sadArray.size)
+            tempArray.toIntArray()
         else convertToHappy(tempArray.toIntArray())
     }
 }
