df5fdaf53997f00d78f3d004300e55b63f661a4f
fusion-steel <pavelgently@gmail.com>
2021-05-25 20:47:32 +0300

    Add Task1StringParser solution.

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,41 @@
 package subtask3
 
 class StringParser {
-
-    // TODO: Complete the following function
+    /**
+     * This function returns parsed substrings from given string,
+     * which lies between these brackets (*), <*>, [*]
+     */
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val bracket = mapOf(
+            '(' to ')',
+            '<' to '>',
+            '[' to ']')
+
+        val resultArray = ArrayList<String>()
+        var index = 0
+
+        while (index < inputString.length) {
+            val currentChar = inputString[index]
+            if (currentChar in bracket.keys) {
+                val begin = index + 1
+                var end: Int
+                var countOpenBracket = 1
+                val closeTag = bracket[currentChar]
+
+                for (i in begin..inputString.length) {
+                    if (inputString[i] == currentChar) ++countOpenBracket
+                    if (inputString[i] == closeTag && countOpenBracket == 1) {
+                        end = i
+                        index = begin - 1
+                        resultArray.add(inputString.substring(begin, end))
+                        break
+                    }
+
+                    if (inputString[i] == closeTag && countOpenBracket != 1) --countOpenBracket
+                }
+            }
+            index++
+        }
+        return resultArray.toTypedArray()
     }
 }
